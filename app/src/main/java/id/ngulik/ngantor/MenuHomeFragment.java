f2adc4abf0882e7ebd36615c3d10d4406b84910d
indrakrisna <indraunikom16@gmail.com>
2019-07-16 01:26:37 +0700

    layouting

--- app/src/main/java/id/ngulik/ngantor/MenuHomeFragment.java
+++ app/src/main/java/id/ngulik/ngantor/MenuHomeFragment.java
@@ -22,8 +22,12 @@ public class MenuHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_menu_home, container, false);
+
+
+
+        return view;
     }
 
 }
--- app/src/main/java/id/ngulik/ngantor/MenuStatFragment.java
+++ app/src/main/java/id/ngulik/ngantor/MenuStatFragment.java
@@ -27,14 +27,6 @@ public class MenuStatFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_menu_stat, container, false);
 
-        Spinner spinner = (Spinner) view.findViewById(R.id.spnMount);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
 
         // Inflate the layout for this fragment
         return view;
